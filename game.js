6d09580f69328a09901c87601338881b4db4e276
thenewadeel <thenewadeel@gmail.com>
2020-01-27 01:11:28 +0500

    NOVA IT IS

---
+++ game.js
@@ -0,0 +1,81 @@
+/// <reference path="./node_modules/@types/p5/global.d.ts"/>
+
+let ROTATION_ANGLE = 0.01;
+let MAX_VEL = 5;
+let GRAVITY_MAGNITUDE=0;
+class Rocket {
+  constructor() {
+      rectMode(CENTER)
+    this.gravity = createVector(0, GRAVITY_MAGNITUDE);
+    this.vel = createVector(0, 0.23);
+    this.pos = createVector(
+      random(width * 0.2, width * 0.8),
+      random(height * 0.2, height * 0.1)
+    );
+    this.thruster = false;
+    // this.thrusterForceDefault = createVector(0, -0.25);
+    // this.thrusterForceDefault = createVector(0, -0.25);
+    // this.thrusterForce = this.thrusterForceDefault;
+  }
+  checkInputAndMove() {
+    if (keyIsDown(LEFT_ARROW)) {
+      this.vel.rotate(-ROTATION_ANGLE);
+    } else if (keyIsDown(RIGHT_ARROW)) {
+      this.vel.rotate(ROTATION_ANGLE);
+    }
+    if (keyIsDown(UP_ARROW)) {
+      this.thruster = true;
+      this.vel.mult(1.125);
+      this.thrusterState = "ON";
+    }else if(keyIsDown(DOWN_ARROW)){
+        // this.thrusterForce.mult(0.8)
+        if(this.vel.mag()>0.1){
+
+            this.vel.mult(0.95)
+        }
+        this.thrusterState = "OFF";
+    // }else {
+    }
+
+    this.vel.add(this.gravity);
+    this.vel.limit(MAX_VEL);
+    if (this.thruster) {
+      this.thruster = false;
+    } else {
+    }
+    this.pos.add(this.vel);
+    // this.thrusterForce = this.thrusterForceDefault;
+  }
+  checkEdges() {
+    if (this.pos.x < 0) {
+      this.pos.x = width;
+    } else if (this.pos.x > width) {
+      this.pos.x = 0;
+    }
+    if (this.pos.y < 0) {
+      this.pos.y = height;
+    } else if (this.pos.y > height) {
+      this.pos.y = 0;
+    }
+  }
+  tick() {
+    this.checkInputAndMove();
+    
+    this.checkEdges();
+  }
+  show() {
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate( this.vel.heading());
+    rect(0,0, 60, 10);
+    // let tt=this.vel.copy().normalize().rotate(this.vel.heading())
+    stroke(200)
+    // line( 25*tt.x, 25*tt.y,0,0)
+    pop();
+    // line(-5+midX / 10, 40, 105+midX / 10, 40)
+    text("Velocity: " + this.vel.mag(), midX / 10, 50);
+    text("Heading: " + Math.floor(this.vel.heading()), midX / 10, 70);
+    text("Thruster: " + this.thrusterState, midX / 10, 90);
+    text("Pos: " + Math.floor(this.pos.x)+", " + Math.floor(this.pos.y), midX / 10, 110);
+  }
+}
--- sketch.js
+++ sketch.js
@@ -1,63 +1,22 @@
-/// < reference path="./p5.global-mode.d.ts" / >
+/// <reference path="./node_modules/@types/p5/global.d.ts"/>
+
+// import { Rocket } from "./game";
+
 
 let canvas, midX, midY;
-let rocket = {};
-let gravity;
-let MAX_VEL = 15;
-rocket.init = function() {
-  this.vel = createVector(0, 0);
-  this.pos = createVector(
-    random(width * 0.2, width * 0.8),
-    random(height * 0.2, height * 0.1)
-  );
-  this.rotation = 0;
-  this.thruster = false;
-  this.thrusterForce = createVector(0, -0.25);
-};
-rocket.checkInput=function(){
-    if(keyIsDown(LEFT_ARROW)){
+let rocket;
 
-    }
-    if(keyIsDown(UP_ARROW)){
-        this.thruster=true;
-    }
-}
-rocket.checkEdges = function() {
-  if (this.pos.x < 0) {
-    this.pos.x = width;
-  } else if (this.pos.x > width) {
-    this.pos.x = 0;
-  }
-  if (this.pos.y < 0) {
-    this.pos.y = height;
-  } else if (this.pos.y > height) {
-    this.pos.y = 0;
-  }
-};
-rocket.tick = function() {
-  this.checkInput();
-  this.vel.add(gravity);
-  this.vel.limit(MAX_VEL);
-  if (this.thruster) {
-    this.vel.add(this.thrusterForce);
-    this.thruster = false;
-  }
-  this.pos.add(this.vel);
-  this.checkEdges();
-};
-rocket.show = function() {
-  rect(this.pos.x, this.pos.y, 10, 50);
-  text("Vel: " + this.vel.mag(), midX / 10, 50);
-};
+
+
 function preload() {}
 function setup() {
-  console.log("setup init");
-  gravity = createVector(0, 0.15);
-  canvas = createCanvas(window.innerWidth * 0.9, window.innerHeight * 0.9);
-  midX = width * 0.5;
-  midY = height * 0.5;
-  background(125);
-  rocket.init();
+    console.log("setup init");
+    
+    canvas = createCanvas(window.innerWidth * 0.9, window.innerHeight * 0.9);
+    midX = width * 0.5;
+    midY = height * 0.5;
+    background(125);
+    rocket= new Rocket();
 }
 function draw() {
   background(frameCount % 255, 127, 25);
